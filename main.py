7c1a02a10bc2b5ef6c2fed2bf6bce3d75ba65461
Pallav <69533542+PallavRastogi@users.noreply.github.com>
2021-10-01 01:24:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 import time
-iport random
-oints = 0
-l1 = [0, 1]
-nq = 0
-woans = 0
-qcount = 1te.sleep(2)
-gamestartend = input("\npress enter to start\n")
-ii = time.time()
-wile 1:
-   fin = time.time()
-   if fin - ini >= 60:
-       break
-    opctrl = random.choice(l1)
-   if opctrl == 1:
-       oppr = "+"
-       op = (lambda a, b: a+b)
-   elif opctrl == 0:
-       oppr = "-"
-       op = (lambda a, b: a-b)
-    no1 = random.randrange(10, 50)
-   no2 = random.randrange(60, 100)
-    print("------------\nQUESTION", qcount, "\n------------\n")
-   qcount += 1
-    print(f"What is {no2} {oppr} {no1}")
+import random
 
-   corans = op(no2, no1)
-   userinp = int(input("enter your answer -\n"))
+points = 0
+l1 = [0, 1]
+noq = 0
+wroans = 0
+qcount = 1
+
+print("WELCOME TO MY GAME\nINSTRUCTIONS:-\nYOU HAVE TO SOLVE AS MANY MATH PROBLEMS AS YOU CAN IN 1 MINUTE TIME\n"
+      "EACH CORRECT ANSWER GIVES YOU 20 POINTS BUT EACH INCORRECT ANSWER DEDUCTS 5 POINTS\n")
+time.sleep(2)
+gamestartend = input("\npress enter to start\n")
+ini = time.time()
+while 1:
+    fin = time.time()
+    if fin - ini >= 60:
+        break
+
+    opctrl = random.choice(l1)
+    if opctrl == 1:
+        oppr = "+"
+        op = (lambda a, b: a+b)
+    elif opctrl == 0:
+        oppr = "-"
+        op = (lambda a, b: a-b)
+
+    no1 = random.randrange(10, 50)
+    no2 = random.randrange(60, 100)
+
+    print("------------\nQUESTION", qcount, "\n------------\n")
+    qcount += 1
+    print(f"What is {no2} {oppr} {no1}")
+    # noinspection PyUnboundLocalVariable
+    corans = op(no2, no1)
+    userinp = int(input("enter your answer -\n"))
+
     if userinp == corans:
-       print("\nCorrect, +20 Points!!\n")
-       noq += 1
-       points += 20
-   else:
-       print("\nIncorrect, -5 Points\n")
-       points -= 5
-       wroans += 1
-pint("--------------\nTIME'S UP!!!\n--------------")
+        print("\nCorrect, +20 Points!!\n")
+        noq += 1
+        points += 20
+    else:
+        print("\nIncorrect, -5 Points\n")
+        points -= 5
+        wroans += 1
+print("--------------\nTIME'S UP!!!\n--------------")
 print()
-pint(f"YOU SCORED {points} POINTS-\nATTEMPTED {noq + wroans} QUESTIONS,\n\nANSWERED {noq} QUESTIONS CORRECTLY   ,AND\n         {wroans} QUESTIONS INCORRECTLY")
-gmestartend = input("\npress enter to exit")
+print(f"YOU SCORED {points} POINTS-\nATTEMPTED {noq + wroans} QUESTIONS,\n\nANSWERED {noq} QUESTIONS CORRECTLY   ,AND\n         {wroans} QUESTIONS INCORRECTLY")
+gamestartend = input("\npress enter to exit")
